393180951f76d4ba689a473188c45bcd56db22ee
rizaldi <rizaldi.95@gmail.com>
2017-09-25 13:56:07 +0700

    edit page home awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ini Home Aplikasi
+                    Ini Home Aplikasi Makan
                 </div>
 
                 <div class="links">
